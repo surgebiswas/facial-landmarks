2b8b608db7009d23da08803a91031eb85b415b68
Surge Biswas <surojitbiswas@g.harvard.edu>
2020-04-04 09:50:21 -0400

    update

--- basic_flask_app.py
+++ basic_flask_app.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import requests
 
 from flask import Flask, flash, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
@@ -11,7 +12,7 @@ import mask
 
 
 UPLOAD_FOLDER = './images/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -23,33 +24,51 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            input_file = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            output_file = os.path.join(app.config['UPLOAD_FOLDER'], 
-                    secure_filename(input_file) + '_mask.jpg')
+        # remove contents of UPLOAD_FOLDER, which will have previous data
+        ufiles = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*'))
+        for uf in ufiles:
+            os.remove(uf)
+        
+        json_content = request.json
+        
+        # If we got sent a json POST of an image
+        # url, then deal with that.
+        if json_content is not None:
+            img_url = json_content['img_url']
+            input_file = os.path.join(app.config['UPLOAD_FOLDER'], os.path.basename(img_url))
             
-            # remove contents of UPLOAD_FOLDER, which will have previous data
-            ufiles = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*'))
-            for uf in ufiles:
-                os.remove(uf)
+            img_data = requests.get(img_url).content
+            with open(input_file, 'wb') as handler:
+                handler.write(img_data)
+               
+        else:
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
 
+            file = request.files['file']
+
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            
+            assert file is not None
+            input_file = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(input_file)
-            
-            # DO IMAGE PROCESSING HERE
-            mask.impose_mask(input_file, output_file)
-            
-            return send_from_directory(app.config['UPLOAD_FOLDER'], 
-                    os.path.basename(output_file), mimetype='image/jpg')
+        
+        
+        print(input_file)
+        output_file = os.path.join(app.config['UPLOAD_FOLDER'], 
+                secure_filename(input_file) + '_mask.jpg')        
+
+        # DO IMAGE PROCESSING HERE
+        mask.impose_mask(input_file, output_file)
+
+        return send_from_directory(app.config['UPLOAD_FOLDER'], 
+                os.path.basename(output_file), mimetype='image/jpg')
             
             
     return '''
